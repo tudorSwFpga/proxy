194022628402bf15c5df4769c72f0005babf13c3
Tudor Constantinescu <tudor.constantinescu@valeo.com>
2022-06-20 23:19:36 +0200

    fix some code and include .cpp in header in order to make the templates work

--- dataManager.hpp
+++ dataManager.hpp
@@ -1,5 +1,8 @@
 #include <plog/Log.h>
 #include <queue>
+#include <array>
+#ifndef DATA_MANAGER_H
+#define DATA_MANAGER_H
 #include <mutex>
 #include "runnable.hpp"
 
@@ -9,22 +12,35 @@
 
 enum MODE { broadcast, map};
 
-template<class T> class queueWrapper;
+//wrapper around the std::queue class
+template<class T>
+class queueWrapper:public std::queue<T> {
+	
+	public:
+		std::string m_id; //name of the app writing in the queue or reading from the queue
+
+		queueWrapper(): std::queue<T>(),m_id(""){
+
+		}
+		
+		~queueWrapper(){
+
+		}
+
+};
 
 template<class T>
 class dataManager: public runnable
 {
 
-using dataVector_t = queueWrapper<T>;
-
 public:
-	dataManager(uint8_t inQueues,uint8_t outQueues): m_nbInQueues(inQueues),m_nbOutQueues(outQueues)
+	dataManager(uint8_t inQueues,uint8_t outQueues): m_nbInQueues(inQueues),m_nbOutQueues(outQueues), m_mode(broadcast)
 	{
 		PLOG_DEBUG << "Constructor; building " << m_nbInQueues << " input queues and "
 		           <<  m_nbOutQueues << " output queues";
 
-		m_inQueues = new dataVector_t[m_nbInQueues];
-		m_inQueues = new dataVector_t[m_nbOutQueues];
+		m_inQueues = new queueWrapper<T>[m_nbInQueues];
+		m_inQueues = new queueWrapper<T>[m_nbOutQueues];
 	}
 
 	~dataManager(){
@@ -33,44 +49,37 @@ public:
 	}
 
 
-	void push(T& data, std::string& pushId);
-	void pop(std::string& pushId, T* data);
+	void push(T&& data, const std::string& pushId);
+	void pop(const std::string& pushId, T* data);
+	//connect a feeder (get an id) to an input queue, if available
+	bool  setFeeder(const std::string& appId);
+	//connect a consumer (get an id) to an output queue, if available
+	bool  setConsumer(const std::string& appId); 
+
 
 private:
 	const uint8_t m_nbInQueues;
 	const uint8_t m_nbOutQueues;
-	const uint8_t m_queueDepth; 
 
-	dataVector_t * m_inQueues;
-	dataVector_t * m_outQueues;
+	queueWrapper<T> * m_inQueues;
+	queueWrapper<T> * m_outQueues;
 	// used to monitor when queue starts to be filling up quickly
 	//have here a routing table variable or some mode 
 	const MODE m_mode;
 
-	void manage(uint8_t pollPeriod);
-	void manageBroadcast();
-	void manageMap();
 	//mutexes for protecting acess to input / output queues
+
 	std::mutex m_inQueuesMutex;
 	std::mutex m_outQueuesMutex;
 
-};
-
-
-//wrapper around the std::queue class
-template<class T>
-class queueWrapper:public std::queue<T> {
-	
-	public:
-		std::string id; //name of the app writing in the queue or reading from the queue
-
-		queueWrapper(std::string queueId): std::queue<T>(),id(queueId){
-
-		}
-		
-		~queueWrapper(){
-
-		}
+	void manage(uint8_t pollPeriod);
+	void manageBroadcast();
+	void manageMap();
 
 
 };
+
+#include "dataManager.cpp"
+
+#endif
+
