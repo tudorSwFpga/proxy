d7b6d7f86d2923fc3ae2e93e2a93cb6dd46763c3
Tudor Constantinescu <tudor.constantinescu@valeo.com>
2022-06-20 23:20:25 +0200

    add data manager in the main and point towawrds it in the tcp server constructor

--- main.cpp
+++ main.cpp
@@ -3,14 +3,22 @@
 #include <plog/Log.h>
 #include <plog/Initializers/RollingFileInitializer.h>
 #include "proxy.hpp"
-
+#include "dataManager.hpp"
 
 int main()
 {
 	plog::init(plog::debug, "/tmp/test.log", 1000000, 5);
 	/* code */
 	PLOG_INFO << "Starting System";
-	std::unique_ptr<TcpServer> tcpServer = std::make_unique<TcpServer>();
+	//create data manager
+	std::shared_ptr<dataManager<std::string>> dataM = std::make_shared<dataManager<std::string>>(1,2);
+	//set interfaces for the data manager
+	dataM->setFeeder("TCPServer");
+	dataM->setConsumer("FlightCalculator");
+	dataM->setConsumer("CollisionCalculator");
+	//create tcp server 
+	std::unique_ptr<TcpServer> tcpServer = std::make_unique<TcpServer>(dataM);
+
 	/*while (true) {
 		tcpServer -> manageConnections();
 	}*/
